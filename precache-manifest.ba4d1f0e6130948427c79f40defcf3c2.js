91844b229cddaa72b1d74b1bf3b6ffcdbcf465fd
Jorge <julianacero98@gmail.com>
2019-04-30 23:51:06 -0500

    round 2

---
+++ precache-manifest.ba4d1f0e6130948427c79f40defcf3c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3acee651bb844d98f60f767a770e0822",
+    "url": "/BreakingBad-ReactHooks-GHPages/index.html"
+  },
+  {
+    "revision": "1988792ad824704fcd69",
+    "url": "/BreakingBad-ReactHooks-GHPages/static/css/main.b0508cf2.chunk.css"
+  },
+  {
+    "revision": "6441df79d5bf7e3f6731",
+    "url": "/BreakingBad-ReactHooks-GHPages/static/js/2.fa25cfbc.chunk.js"
+  },
+  {
+    "revision": "1988792ad824704fcd69",
+    "url": "/BreakingBad-ReactHooks-GHPages/static/js/main.b84b5ea6.chunk.js"
+  },
+  {
+    "revision": "5a668afc0d5c8ed5df9e",
+    "url": "/BreakingBad-ReactHooks-GHPages/static/js/runtime~main.9724c5b0.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/BreakingBad-ReactHooks-GHPages/static/media/logo.427a4adc.svg"
+  }
+]);
